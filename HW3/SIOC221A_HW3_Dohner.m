10b598cf658a66d100cb6b44e1dd76ff2e2e71dc
Julia Dohner <jdohner@cs.hmc.edu>
2017-10-18 21:58:59 -0700

    finishing up hw3 on wednesday night

--- HW3/SIOC221A_HW3_Dohner.m
+++ HW3/SIOC221A_HW3_Dohner.m
@@ -1,6 +1,6 @@
 % file SIOC 221A HW 3
 % 
-% author Julia Dohner
+% author Julia Dohner, with help from Luke Kachelein and Dillon Amaya
 %
 % due date October 19, 2017
 
@@ -22,7 +22,7 @@ pressure = [pressure; ncread(strcat('http://sccoos.org/thredds/dodsC/autoss/scri
 pressure_flagPrimary = [];
 pressure_flagPrimary = [pressure_flagPrimary; ncread(strcat('http://sccoos.org/thredds/dodsC/autoss/scripps_pier-2015.nc'),'pressure_flagPrimary')];
 
-%looping through to remove bad data from pressure record
+% looping through to remove bad data from pressure record
 for i = 1:length(pressure)
     if pressure_flagPrimary(i) ~= 1
         pressure(i) = nan;
@@ -46,13 +46,8 @@ set(gca,'FontSize',16);
 title('Scripps Pier Pressure');
 xlabel('Date');
 datetick('x','mm/dd/yy')
-ylabel('dbar');
+ylabel('Pressure (dbar)');
 
-
-% The data are for the most part uniformly spaced by 361 seconds, aside
-% from later in the record where there seems to be a huge break of 17 days
-% (max(X) = 1476982 seconds), and some places where the data is taken at
-% closer intervals (min(X) = 220 seconds)
 
 %% plotting just the first month of 2015
 
@@ -66,14 +61,13 @@ pressure2 = pressure(1:7180);
 % plot the time series
 date0=datenum(1970,1,1); % give reference date (first date)
 time4 = double(time3)/24/3600+date0; % divide the time by 24*3600 to convert seconds into days since 1970
+time4_string = datestr(time4);
 figure('name','Scripps_Pier_Pressure_January_2015');
-plot(time4, pressure2,'LineWidth',1);
-% TODO: format x axis date ticks
-title('Scripps Pier Pressure in Janury 2015');
+plot(1:length(pressure2),pressure2,'LineWidth',1);
+title('Scripps Pier Pressure in January 2015');
 xlabel('Date');
-set(gca, 'XTickMode', 'auto', 'XTickLabelMode', 'auto')
-datetick('x','mm/dd/yy')
-ylabel('dbar');
+set(gca, 'xtick', 1:1000:length(pressure2), 'xticklabel', time4_string(1:1000:length(pressure2),1:6));
+ylabel('Pressure (dbar)');
 
 %% Least squares fit
 
@@ -88,24 +82,19 @@ M2_sin = sin(2*pi*time4/(12.42/24)); %M2: principal lunar
 M2_cos = cos(2*pi*time4/(12.42/24));
 
 
-% TODO: do I want the time4(:) in here? I took it out of second column of A
-% matrix. Got rid of the error when I removed it
-
 A2=[ones(length(time4),1) O1_sin O1_cos K1_sin K1_cos M2_sin M2_cos];
 x2=inv(A2'*A2)*A2'*pressure2;
 figure('name','Pier_Pressure_Tidal_LSF');
-plot(time4,A2*x2,'m','LineWidth',2)
+matrixProd = A2*x2;
+plot(1:length(matrixProd),matrixProd,'LineWidth',1);
 hold on
-plot(time4, pressure2,'LineWidth',1);
-set(gca,'FontSize',16);
+plot(1:length(pressure2),pressure2,'LineWidth',1); 
+set(gca, 'xtick', 1:1000:length(pressure2), 'xticklabel', time4_string(1:1000:length(pressure2),1:6));
 title('Pier Pressure Least Squares Fit of 3 Major Tidal Constituents');
 xlabel('Time');
-datetick('x','mm/dd/yy')
-ylabel('TODO');
+ylabel('Pressure (dbar)');
 
 
-% The mean is 3.4898 (first row in x2 vector)
-% 
 % Total amplitude = square root of the sum of the squares of the sine and 
 % cosine amplitudes)
 % Units of mean and amplitude are decibars
@@ -115,7 +104,7 @@ amplitude_M2_jan = sqrt((x2(6,1))^2 + (x2(7,1))^2);
 
 %% Stationarity of the tide
 
-% repeating the least squares fit for 30 days roughly near August 2015 
+% Repeating the least squares fit for 30 days roughly near August 2015 
 
 % Starting 7/12 of the way through the time record (82237 measurements).
 % If a measurement is taken every 361 seconds, then 30 days into the record
@@ -146,9 +135,10 @@ hold on
 plot(timeAugust, pressureAugust,'LineWidth',1);
 set(gca,'FontSize',16);
 title('Pier Pressure Least Squares Fit of 3 Major Tidal Constituents - Summer');
-xlabel('Time');
+xlabel('Date');
 datetick('x','mm/dd/yy')
-ylabel('TODO');
+ylabel('Pressure (dbar)');
+
 
 % The mean is 3.4898 (first row in x2 vector)
 % 
@@ -159,35 +149,68 @@ amplitude_O1_aug = sqrt((x3(2,1))^2 + (x3(3,1))^2);
 amplitude_K1_aug = sqrt((x3(4,1))^2 + (x3(5,1))^2);
 amplitude_M2_aug = sqrt((x3(6,1))^2 + (x3(7,1))^2);
 
-% The tidal amplitudes for the K1 tide decreases by 0.07. The amplitude of 
-% M2 decreases by 0.01, and the amplitude of O1 does not change. The tidal 
-% component that changes the most is the K1 tide. Because the earth rotates 
-% on a tilted axis relative to our axis of rotation around the sun, the
-% distance of the pier from the sun changes on a seasonal scale, changing
-% the influence of solar forcing on local tides. The K1 tide is the only of
-% the three tides that contains a component reflecting the influence of the
-% sun on tides, thus it makes sense that K1 shows the biggest change in
-% amplitude between January and August.
 
 %% Chi squared and the misfit. (Good name for a short story)
 
 % y is vector containing pressure data (pressureAugust)
 % A is the matrix containing ones, sines and cosines (A3)
 % x is matrix containing mean and amplitudes (x3)
-% TODO: Question: is sigma the uncertainty for each point? Or sigma for the
-% whole data?
+
 sigma = std(pressureAugust);
+% my first attempt at looping through to sum:
+
 %sum = symsum((((pressureAugust - A3*x3).^2)/sigma.^2), 1, length(pressureAugust));
 
-chiSquared = 0; % initialized chiSquared variable before loop
-for k = 1:length(pressureAugust)
-    chiSquared = chiSquared + (pressureAugust(k) - (A3*x3)
+% initializing variables before loop
+chiSquared = 0; 
+ax_prod = 0;
+for i = 1:length(pressureAugust)
+    for j = 1:length(x3)
+        ax_prod = ax_prod + A3(i,j)*x3(j);
+    end
+    chiSquared = chiSquared + ((pressureAugust(i) - ax_prod)^2);
+end
+chiSquared = chiSquared/(sigma^2);
+
+% How much does the misfit change if you fit with 5 frequencies instead of
+% 3?
+
+% convert period to days (to match x axis time units)
+S2_sin_aug = sin(2*pi*timeAugust/(12/24)); %S2: principal solar semidiurnal
+S2_cos_aug = cos(2*pi*timeAugust/(12/24)); 
+N2_sin_aug = sin(2*pi*timeAugust/(12.66/24)); %N2: larger lunar elliptic semidiurnal
+N2_cos_aug = cos(2*pi*timeAugust/(12.66/24));
+
+
+A4=[ones(length(timeAugust),1) O1_sin_aug O1_cos_aug K1_sin_aug ...
+    K1_cos_aug M2_sin_aug M2_cos_aug S2_sin_aug S2_cos_aug N2_sin_aug N2_cos_aug];
+x4=inv(A4'*A4)*A4'*pressureAugust;
+
+% recalculate misfit now with the 5-frequency fit (using A4 and x4)
+
+chiSquared_5 = 0; 
+ax_prod_5 = 0;
+for i = 1:length(pressureAugust)
+    for j = 1:length(x4)
+        ax_prod_5 = ax_prod_5 + A4(i,j)*x4(j);
+    end
+    chiSquared_5 = chiSquared_5 + ((pressureAugust(i) - ax_prod_5)^2);
+end
+chiSquared_5 = chiSquared_5/(sigma^2);
 
 
 
+% How could you decide if the reduced misfit was sufficient to justify
+% fitting additional frequencies?
 
+% gammainc gives the probability that purely by chance we get a value
+% that's as good as we can find
+% if p is close to 1, the data is too good to be true
+% essentially asks if we're overfitting our data
 
+% p = gammainc(chi_squared/2,nu/2)
 
-
+% TODO: what's the threshold of p that lets us decide if we can fit
+% additional frequencies?
 
 
